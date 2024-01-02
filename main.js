ee0355d1bc02b16cc631874ef6079ca9f5e50939
Wacoder Forever <sethomondi088@gmail.com>
2024-01-02 17:15:51 +0300

    triggers

--- main.js
+++ main.js
@@ -1,10 +1,45 @@
+let page="hello"
+
 function start(the_page){
+    if(page=="hello"){
     let mydiv=the_page.div(()=>{
         the_page.h1("Hello world")
     })
     mydiv.inline_style({
         "color":"red"
     })
+   let mybutton=the_page.button(()=>{
+    the_page.p("Goodbye")
+    the_page.set_prop("render_click",()=>{
+        page="Bye"
+    })
+    the_page.inline_style({
+        "color":"white",
+        "background-color":"black",
+        "border-radius":"50%"
+    })
+   })
+}
+    if(page=="Bye"){
+        let mydiv2=the_page.div(()=>{
+            the_page.h1("Goodbye world")
+            the_page.inline_style({
+                "color":"blue"
+            })
+
+        })
+        let mybutton2=the_page.button(()=>{
+            the_page.p("Hello")
+            the_page.set_prop("render_click",()=>{
+                page="hello"
+            })
+            the_page.inline_style({
+                "color":"white",
+                "background-color":"black",
+                "border-radius":"50%"
+            })
+        })
+    }
 }
 
 function main(){
